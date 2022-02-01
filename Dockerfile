080263000c3a9f2837b62cc910e3242fa2e01cf5
AD-TECOTEC\tanaka.yoshitaka <tanaka.yoshitaka@tecotec.co.jp>
2022-02-01 19:19:58 +0900

    お試しで

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ FROM golang:latest
 RUN mkdir -p /go/src/github.com/44taka/golang-gin
 RUN go get -u github.com/gin-gonic/gin
 RUN go get -u github.com/cosmtrek/air
+RUN go get -u gorm.io/driver/mysql
+RUN go get -u gorm.io/gorm
 
 WORKDIR /go/src/github.com/44taka/golang-gin
--- app/main.go
+++ app/main.go
@@ -1,12 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+  
+type User struct {
+	ID  int
+	Name string
+}
 
 func main() {
+
+	dsn := "root:root@tcp(mysql:3306)/my_testdb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Error!!")
+		return
+	}
+	var user User
+	// var users []User
+	fmt.Println(db.First(&user))
+	// fmt.Println(db.Raw("select * from user limit 1").Scan(&user).Name)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+			"test": "test!!",
+			"test2": "tesAAsdfasfasefaaaaaataaa",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
